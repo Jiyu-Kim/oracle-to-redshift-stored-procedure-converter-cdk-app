80023678c6e3a4c40850ce3280a23a458177150a
jiyu-kim <jiyukim@amazon.com>
2024-08-28 10:56:57 +0900

    [Fix] Set default account and region

--- cdk-app/app.py
+++ cdk-app/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-import os
-from dotenv import load_dotenv
 
+import boto3
 import aws_cdk as cdk
 
 from cdk_stacks.redshift_serverless_stack import RedshiftServerlessStack
@@ -10,9 +9,15 @@ from cdk_stacks.sagemaker_notebook_stack import SagemakerNotebookStack
 
 app = cdk.App()
 
+# Retrive AWS account ID and region
+session = boto3.Session()
+sts_client = session.client('sts')
+account_id = sts_client.get_caller_identity()["Account"]
+region = session.region_name
+
 default_env = {
-    "account": os.getenv("ACCOUNT"),
-    "region": os.getenv("REGION_NAME", "ap-northeast-2")
+    "account": account_id,
+    "region": region
 };
 
 redshift_admin_user = "<admin_user>"
